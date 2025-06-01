bff0d22c810dc0a673f5651e6d4fa29ee440b5b3
Michael Mikhailov <yohanson@users.noreply.github.com>
2025-06-02 02:40:56 +0700

    update colors

--- lua/yohanson/colors.lua
+++ lua/yohanson/colors.lua
@@ -1,5 +1,5 @@
 -- Two colorschemes intentionally, result differs from single.
--- vim.cmd.colorscheme("habamax")
+vim.cmd.colorscheme("habamax")
 vim.cmd.colorscheme("PaperColor")
 vim.api.nvim_set_hl(0, "Normal", { ctermbg=235 }) -- a little lighter background
 vim.api.nvim_set_hl(0, "Visual", { cterm=reverse, ctermfg=16, ctermbg=103 })
@@ -12,5 +12,4 @@ vim.cmd([[highlight htmlError guifg=None guibg=None]])
 
 vim.api.nvim_set_hl(0, '@lsp.type.comment', {})
 
--- vim.cmd("set notermguicolors")
 vim.o.termguicolors = false
